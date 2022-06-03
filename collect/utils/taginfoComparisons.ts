6d2e569f02ab7b04f327b948d081a2ecd8d1571b
wouter van der plas <2423856+wvanderp@users.noreply.github.com>
2022-06-03 10:38:23 +0200

    fixed more errors

--- collect/utils/taginfoComparisons.ts
+++ collect/utils/taginfoComparisons.ts
@@ -4,12 +4,16 @@ import { Comparison } from '../types';
 export default async function taginfoComparisons(
     name: string,
     key: string,
-    value:string,
+    value: string,
     expected: number,
     expectedSource: string,
     description: string,
     server = 'https://taginfo.openstreetmap.org'
 ): Promise<Comparison> {
+    if (server[server.length - 1] === '/') {
+        server = server.slice(0, -1);
+    }
+
     const count = await taginfo(key, value, server);
 
     return {
