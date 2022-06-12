0d8ce3d4a08f7f131e22ec447c962879d30f0370
Wouter van der Plas <2423856+wvanderp@users.noreply.github.com>
2022-06-12 21:54:30 +0200

    sorted the countries

--- app/App.tsx
+++ app/App.tsx
@@ -3,12 +3,27 @@ import { CountryCodes } from '../collect/types';
 import data from '../data/compare.json';
 import Country from './Country';
 
+const prioritizedCountries = new Set([
+    'worldwide',
+    'EU'
+]);
+
 export default function App() {
     const countries = Object.entries(data.comparisons)
+        .sort((a, b) => {
+            if (prioritizedCountries.has(a[0]) && !prioritizedCountries.has(b[0])) {
+                return -1;
+            }
+            if (prioritizedCountries.has(b[0]) && !prioritizedCountries.has(a[0])) {
+                return 1;
+            }
+            return (a[0] === b[0] ? 1 : -1);
+        })
         .map((country) => <Country key={country[0]} code={country[0] as CountryCodes} comparisons={country[1]} />);
 
     return (
         <>
+            <h1>is osm complete?</h1>
             <p>this site tracks how complete osm really is</p>
             {countries}
         </>
